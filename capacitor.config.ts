c365eee7ff6b6c0504b30619976e2a7c7d85ab80
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-03-17 00:44:21 +0100

    changed stuff

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,19 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'BowlingStats',
   webDir: 'www',
+  bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    "SplashScreen": {
+        "launchShowDuration": 3000,
+        "launchAutoHide": true,
+        "androidScaleType": "CENTER_CROP",
+        "splashImmersive": true,
+        "backgroundColor": "#ffffff"
+    }
+}
 };
 
 export default config;

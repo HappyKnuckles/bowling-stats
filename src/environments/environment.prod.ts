7d951ed536f67e87d1fad4b700a84ade530f17d7
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-03-29 00:58:24 +0100

    Fixed

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  key: "401521a17e39486a8837f6b31c265f56",
+  endPoint: "https://happyknuckles.cognitiveservices.azure.com/",
+  sasUrl: "https://happyknucklesimages.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2029-03-23T23:59:30Z&st=2024-03-23T15:59:30Z&spr=https&sig=7z6AssmXSPNU%2BdsZPDHd1ml9IlpxTCIOoxzFaLmtLwg%3D",
+  imagesUrl: "https://happyknucklesimages.blob.core.windows.net/images/"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  key: "401521a17e39486a8837f6b31c265f56",
+  endPoint: "https://happyknuckles.cognitiveservices.azure.com/",
+  sasUrl: "https://happyknucklesimages.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2029-03-23T23:59:30Z&st=2024-03-23T15:59:30Z&spr=https&sig=7z6AssmXSPNU%2BdsZPDHd1ml9IlpxTCIOoxzFaLmtLwg%3D",
+  imagesUrl: "https://happyknucklesimages.blob.core.windows.net/images/"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.

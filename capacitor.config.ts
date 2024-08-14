23c8b387534d53e29c4c56b44a388c599322f23a
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-08-15 00:08:03 +0200

    Changed to esbuild

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.bowling',
   appName: 'BowlingStats',
-  webDir: 'www/browser',
+  webDir: 'www/browser/browser',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'

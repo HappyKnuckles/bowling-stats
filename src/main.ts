80f441c2e046820e45a321cac90e089b494c4b92
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-09-06 14:50:01 +0200

    Added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { environment } from './environments/environment';
 import { RouteReuseStrategy } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular, IonApp, IonBackdrop, IonSpinner, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
@@ -12,10 +9,13 @@ import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http'
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { AppComponent } from './app/app.component';
 import { provideServiceWorker } from '@angular/service-worker';
+import { inject } from '@vercel/analytics';
 
 if (environment.production) {
   enableProdMode();
 }
+
+inject();
 
 bootstrapApplication(AppComponent, {
   providers: [
